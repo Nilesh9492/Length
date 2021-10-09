b90ea1ba673bc7da79c4ad5c40bf1545ef81e881
Nilesh9492 <nilesh94041@gmail.com>
2021-10-09 09:32:22 +0530

    Add project files.

---
+++ Geometry/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Geometry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Enter coordinates x and y respctively of point A:");
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter coordinates x and y respctively of point A:");
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x = x2 - x1;
+            double y = y2 - y1;
+            double pow_x = Math.Pow(x,2);
+            double pow_y = Math.Pow(y, 2);
+            double z = pow_x + pow_y;
+            Console.WriteLine("Length of Line is:" + Math.Sqrt(z));
+
+
+
+        }
+    }
+}
